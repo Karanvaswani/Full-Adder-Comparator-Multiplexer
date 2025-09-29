8795e7374b0645d204e75b32ee54968e93e3d35a
karan kumar Maheshwari <166040532+Karanvaswani@users.noreply.github.com>
2025-09-29 10:08:02 +0500

    4 input  MUX.vhd

--- 4_1 MUX.vhd
+++ 4_1 MUX.vhd
@@ -1,95 +1,17 @@
--------------------------Data Flow using When/else---------------------------
-library ieee;
-use  ieee.std_logic_1164.all;
-
-entity 4_1M is 
-Port(a,b,c,d,s0,s1: in Std_logic;
-     y : out std_logic);
-end 4_1M;
-
-Architecture Dataflow of 4_1M is
-begin
-y<= a when (s1='0' and s0='0') else
-    b when (s1='0' and s0='1') else
-    c when (s1='1' and s0='0') else
-    d;
-end dataflow;
-
-------------------------Data Flow using When/else(Array Selection)-------------------------------
-
-library ieee;
-use  ieee.std_logic_1164.all;
-
-entity 4_1M is 
-Port(a,b,c,d: in Std_logic;
-     s:in std_logic_vector(1 downto 0);
-     y : out std_logic);
-end 4_1M;
-
-Architecture Dataflow of 4_1M is
-begin
-y<= a when s="00" else
-    b when s="01" else
-    c when s="10" else
-    d;
-end dataflow;
-
-------------------------Data Flow using With/Select/When -------------------------------
-
-library ieee;
-use  ieee.std_logic_1164.all;
-
-entity 4_1M is 
-Port(a,b,c,d: in Std_logic;
-     s:in std_logic_vector(1 downto 0);
-     y : out std_logic);
-end 4_1M;
-
-Architecture Dataflow of 4_1M is
-begin
-With s SELECT
-y<= a when "00",
-    b when "01",
-    c when "10",
-    d;
-end dataflow;
-
-------------------Data Flow using s defined as a Integer----------------------
-
-library ieee;
-use  ieee.std_logic_1164.all;
-
-entity 4_1M is 
-Port(a,b,c,d: in Std_logic;
-     s:in integer range 0 to 3 ;
-     y : out std_logic);
-end 4_1M;
-
-Architecture Dataflow of 4_1M is
-begin
-With s SELECT
-y<= a when 0,
-    b when 1,
-    c when 2,
-    d when 3; -- Where 3 or others are Equivalent
-end dataflow;
-
----------------Data flow using when/else and s as a Integer-----------------------
-
-library ieee;
-use  ieee.std_logic_1164.all;
-
-entity 4_1M is 
-Port(a,b,c,d: in Std_logic;
-     s:in integer range 0 to 3 ;
-     y : out std_logic);
-end 4_1M;
-
-Architecture Dataflow of 4_1M is
-begin
-With s SELECT
-y<= a when s=0 else
-    b when s=1 else
-    c when s=2 else
-    d; 
-end dataflow;
+--4 input multiplexer   
+entity MUX4_20es062 is 
+    Port ( A_062 : in  STD_LOGIC; 
+           B_062 : in  STD_LOGIC; 
+           C_062 : in  STD_LOGIC; 
+           D_062 : in  STD_LOGIC; 
+           S_062 : in  STD_LOGIC_VECTOR (1 downto 0); 
+           Y_062 : out  STD_LOGIC); 
+end MUX4_20es062; 
+architecture Behavioral of MUX4_20es062 is 
+begin 
+with S_062 select 
+Y_062 <= A_062 when "00", 
+         B_062 when "01", 
+         C_062 when "10", 
+         D_062 when others; 
+end Behavioral;
