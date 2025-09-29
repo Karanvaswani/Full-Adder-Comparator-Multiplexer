62d82c20cf718239c282e2c52c6fa08ec0182156
karan kumar Maheshwari <166040532+Karanvaswani@users.noreply.github.com>
2025-09-29 10:13:57 +0500

    4bit_magnitude_comparator.vhd

--- 4bit_magnitude_comparator.vhd
+++ 4bit_magnitude_comparator.vhd
@@ -1,23 +1,33 @@
-Library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
- 
-entity Binary_Comparator is
-  port (
-      A,B   : in STD_LOGIC_VECTOR(3 downto 0);
-      greater, equal, smaller  : out STD_LOGIC);
-end Binary_Comparator ;
- 
-architecture behav of Binary_Comparator is
+entity comparator4 is
+    Port (
+        A_62 : in  STD_LOGIC_VECTOR (3 downto 0); -- 4-bit input A
+        B_62 : in  STD_LOGIC_VECTOR (3 downto 0); -- 4-bit input B
+        C_62 : in  STD_LOGIC_VECTOR (3 downto 0); -- 
+        X_62 : out STD_LOGIC;                     -- A > B
+        Y_62 : out STD_LOGIC;                     -- A = B
+        Z_62 : out STD_LOGIC                      -- A < B
+    );
+end comparator4;
+
+architecture Behavioral of comparator4 is
 begin
-greater <= '1' 
-            when (A > B)
-            else '0';
-equal <= '1'
-            when (A = B)
-            else '0';
-smaller <= '1' 
-            when (A < B)
-            else '0';
-end behav;
-
+    process (A_62, B_62)
+    begin
+        if (A_62 > B_62) then
+            X_62 <= '1';  -- A greater
+            Y_62 <= '0';
+            Z_62 <= '0';
+        elsif (A_62 = B_62) then
+            X_62 <= '0';
+            Y_62 <= '1';  -- A equal
+            Z_62 <= '0';
+        else
+            X_62 <= '0';
+            Y_62 <= '0';
+            Z_62 <= '1';  -- A less
+        end if;
+    end process;
+end Behavioral;
