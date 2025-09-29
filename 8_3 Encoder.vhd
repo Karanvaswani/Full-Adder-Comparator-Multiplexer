d0c18de0414aacc30b0b0c092f2e3e706dbfca81
karan kumar Maheshwari <166040532+Karanvaswani@users.noreply.github.com>
2025-09-29 10:20:02 +0500

    four_bit_full_adder .vhd

--- 8_3 Encoder.vhd
+++ 8_3 Encoder.vhd
@@ -1,50 +1,29 @@
------------------------DataFlow Model using when/else---------------------------
+-- =====================================================
+-- 4-Bit Full Adder in VHDL
+-- File: full_adder4.vhd
+-- =====================================================
 
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;  -- Allows arithmetic with std_logic_vector
 
-entity 8_3E is
-port( x: in std_logic_vector(7 downto 0);
-      y: out Std_logic_vector(2 downto 0));
-      
-end 8_3E;
+entity full_adder_062 is
+    Port (
+        A_62    : in  STD_LOGIC_VECTOR (3 downto 0); -- 4-bit input A
+        B_62    : in  STD_LOGIC_VECTOR (3 downto 0); -- 4-bit input B
+        Cin_62  : in  STD_LOGIC;                     -- Carry input
+        Sum_62  : out STD_LOGIC_VECTOR (3 downto 0); -- 4-bit sum
+        Cout_62 : out STD_LOGIC                      -- Carry output
+    );
+end full_adder4;
 
-Architecture DataFlow of 8_3E is 
+architecture Behavioral of full_adder_062 is
+    signal temp_62 : STD_LOGIC_VECTOR (4 downto 0); -- 5-bit signal to hold result
 begin
-y<= "000" when x="00000001" else
-    "001" when x="00000010" else
-    "010" when x="00000100" else
-    "011" when x="00001000" else
-    "100" when x="00010000" else
-    "101" when x="0010000" else
-    "110" when x="01000000" else
-    "111" when x="10000000" else
-    "ZZZ";
-
-end DataFlow;
-    
------------------------DataFlow Model using with/select/when---------------------------
-
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity 8_3E is
-port( x: in std_logic_vector(7 downto 0);
-      y: out Std_logic_vector(2 downto 0));
-      
-end 8_3E;
-
-Architecture DataFlow of 8_3E is 
-begin
-with x SELECT
-y<= "000" when "00000001",
-    "001" when "00000010",
-    "010" when "00000100",
-    "011" when "00001000",
-    "100" when "00010000",
-    "101" when "00100000",
-    "110" when "01000000",
-    "111" when "10000000",
-    "ZZZ" when OTHERS;
-
-end DataFlow;
+    process (A_62, B_62, Cin_62)
+    begin
+        temp_62 <= ('0' & A_62) + ('0' & B_62) + ("0000" & Cin_62); 
+        Sum_62  <= temp_62(3 downto 0); -- lower 4 bits
+        Cout_62 <= temp_62(4);          -- Most significant bit as carry out
+    end process;
+end Behavioral;
